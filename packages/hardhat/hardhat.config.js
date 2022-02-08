fdb551a519d3b2a6c9031722eab0b6d1dfda3eb1
SireMartin <maarten.deheegher@gmail.com>
2022-02-08 21:31:29 +0100

    make it run here

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -115,6 +115,15 @@ module.exports = {
         }
       },
       {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
         version: "0.6.7",
         settings: {
           optimizer: {
